192789e1314421e1d270632de34dd7dcb5fdd831
Ubuntu <ubuntu@ip-172-31-17-232.us-east-2.compute.internal>
2020-01-06 16:13:55 +0000

    completed assessment

--- ruby.rb
+++ ruby.rb
@@ -5,11 +5,27 @@
 
 myArray = [1, 2, 6, 9, 3, 21]
 
+# can replace '{ }' with 'do' 
+
+myArray.map { |n| print n * 3 }
+
+def mult_3 array
+  array.map { |n| n * 3 }
+end 
+
+print mult_3 myArray
+
 
 
 # 2. Create a method that takes in a sentence and returns a new sentence with the first letter of each word capitalized. Expected output = "Hello There, How Are You?"
 
 sentence = "hello there, how are you?"
+
+def firstcap string
+  string.split(' ').map(&:capitalize).join(' ')
+end 
+
+puts firstcap sentence
 
 
 
@@ -17,29 +33,65 @@ sentence = "hello there, how are you?"
 
 no_vowels = "I have no vowels"
 
+def vowel_remover string
+ string.downcase.delete 'aeiou'
+end 
+
+puts vowel_remover no_vowels
+  
+  
 
 
 # 4. Look at this horrible Ruby code. Fix it to be good Ruby code.
 
-class example
-  constructor(day)
-    @day=day
+class Example
+  def constructor day
+    @day = day
   end
 
-  def SayHi
-    if(day === "Friday"){
+  def say_hi
+    if @day == "Friday"
       puts "TGIF!"
-    }
-    else if(day === "Monday"){
+    elsif @day == "Monday"
       puts "Its monday again"
-    }
-    else{
+    else
       puts "another day"
-    }
+    end
+  end
 end
 
+
+a_day = Example.new
+a_day.constructor('Friday')
+puts a_day.say_hi
+
 # 5a. Create a class called Animal that initializes with a color. Create a method in the class called legs that returns 4.
+
+class Animal 
+  def initialize color = 'default'
+    @color = color
+    @legs = 4
+  end 
+  
+  def legs
+    @legs
+  end 
+  
+  def color
+      @color
+  end 
+  
+  def describe 
+      puts "My animal's color is #{@color} and it has #{@legs} legs."
+  end
+  
+end 
 
 
 
 # 5b. Create a new instance of an Animal with a brown color. Return how the number of legs for the animal object.
+
+my_animal = Animal.new 'brown'
+puts my_animal.legs 
+puts my_animal.color
+puts my_animal.describe
